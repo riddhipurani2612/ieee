2a09fbe03f897090925153eb8d789b8d874b1a51
Prem Raval <50078718+premraval1710@users.noreply.github.com>
2021-02-13 02:24:13 -0500

    Static HomePage Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import Home from "./components/Home";
+import Navigation from "./components/Navigation";
 function App() {
   return (
     <div className="App">
-      
+      <Navigation/>
+      <Home/>
     </div>
   );
 }
