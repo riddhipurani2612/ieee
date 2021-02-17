e9f44f8d62c0bc87db05d62bf454de000636b567
Prem Raval <50078718+premraval1710@users.noreply.github.com>
2021-02-16 22:47:23 -0500

    Feedback Modal and Member Route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import Feedback from "./components/Feedback";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navigation from "./components/Navigation";
 import LoginSignup from "./components/LoginSignupForm";
-import FAQ from "./components/FAQ";
-import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Members from "./components/Members";
 function App() {
     return (
         <Router>
-        <div className="App">
-            <Navigation />
-            <FAQ />
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/login_signup" component={LoginSignup}/>
-                <Route path="/faq" component={FAQ}/>
-            </Switch>
-            <Footer />
-        </div>
+            <div className="App">
+                <Navigation />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/members" component={Members} />
+                    <Route path="/login_signup" component={LoginSignup} />
+                </Switch>
+                <Footer />
+            </div>
         </Router>
     );
 }
