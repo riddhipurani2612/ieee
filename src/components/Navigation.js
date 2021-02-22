6b1457bad265f3c08627228b736248dbe9bc847b
Riddhi Purani <puraniriddhi@gmail.com>
2021-02-22 17:46:29 +0530

    SIGN UP FORM COMMIT

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,64 +2,59 @@ import React from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import logo from "../assets/grss_logo.png";
 const Navigation = (props) => {
-    return (
-        <Navbar expand="lg" bg="light" variant="light" sticky="top">
-            <Navbar.Brand>
-                <img width="7%" height="7%" src={logo} />
-                <span className="mx-3">IEEE Gujarat Section GRSS Chapter</span>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Item>
-                        <Nav.Link href="/">Home</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/chairs_desk">Chair's Desk</Nav.Link>
-                    </Nav.Item>
-                    <NavDropdown title="Members" id="members_dropdown">
-                        <NavDropdown.Item href="/founderMembers">
-                            Founder Members
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="/professionalMembers">
-                            Professional Members
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="/studentMembers">
-                            Student Members
-                        </NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="Material" id="material_dropdown">
-                        <NavDropdown.Item href="/newsletters">
-                            Newsletters
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="/publication">
-                            Publications
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="/lecture">
-                            Lectures
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="/sar">SAR</NavDropdown.Item>
-                        <NavDropdown.Item href="https://www.ieee-grss-gujaratsection.org/tarangsoftware">Download Tarang Software</NavDropdown.Item>
-                        <NavDropdown.Item href="https://www.ieee-grss-gujaratsection.org/tarangsoftware">
-                            Download Tarang Software
-                        </NavDropdown.Item>
-                    </NavDropdown>
-                    <Nav.Item>
-                        <Nav.Link href="/events">Events</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/about">About</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/contact">Contact</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="/login">Login/SignUp</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
+  return (
+    <Navbar expand="lg" bg="light" variant="light" sticky="top">
+      <Navbar.Brand>
+        <img width="7%" height="7%" src={logo} />
+        <span className="mx-3">IEEE Gujarat Section GRSS Chapter</span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/chairs_desk">Chair's Desk</Nav.Link>
+          </Nav.Item>
+          <NavDropdown title="Members" id="members_dropdown">
+            <NavDropdown.Item href="/founderMembers">
+              Founder Members
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/professionalMembers">
+              Professional Members
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/studentMembers">
+              Student Members
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Material" id="material_dropdown">
+            <NavDropdown.Item href="/newsletters">Newsletters</NavDropdown.Item>
+            <NavDropdown.Item href="/publication">
+              Publications
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/lecture">Lectures</NavDropdown.Item>
+            <NavDropdown.Item href="/sar">SAR</NavDropdown.Item>
+            <NavDropdown.Item href="https://16fbc7d9-21b8-46d1-a23a-6d57e5ebf2ea.filesusr.com/archives/f6c427_4a32ef40e06146ff8a9cffb3e9a270e4.rar?dn=TARANG_24Mar2014.rar">
+              Download Tarang Software
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Item>
+            <Nav.Link href="/events">Events</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/login">Login/SignUp</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Navigation;
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 const Styles = styled.div`
   .main-bg {
     background: #2e151b;
-    height: 100vh;
   }
   .text {
     color: white;
@@ -18,7 +17,16 @@ const SignUp = () => {
     setSignupData({ ...signupData, [e.target.name]: e.target.value });
   };
 
-  const { name, address, contactno, email } = signupData;
+  const {
+    fname,
+    lname,
+    role,
+    address,
+    contactno,
+    email,
+    workplace,
+    designation,
+  } = signupData;
 
   const next = (e) => {
     console.log(signupData);
@@ -30,27 +38,50 @@ const SignUp = () => {
         <Container>
           <div>
             <Form className="text">
-            <Form.Group controlID="signup_name">
-                <Form.Label>Name</Form.Label>
+              <Form.Group controlId="signup_name">
+                <Form.Label>First name</Form.Label>
                 <Form.Control
                   type="text"
-                  value={name}
-                  name="name"
-                  placeholder="Enter Name"
+                  value={fname}
+                  name="fname"
+                  placeholder="Enter First Name"
                   onChange={signupValueChanged}
                 ></Form.Control>
               </Form.Group>
-              <Form.Group controlID="signup_address">
+              <Form.Group controlId="signup_name">
+                <Form.Label>Last name</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={lname}
+                  name="lname"
+                  placeholder="Enter Last Name"
+                  onChange={signupValueChanged}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="role">
+                <Form.Label>Choose role</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="role"
+                  value={role}
+                  onChange={signupValueChanged}
+                >
+                  <option selected="true">--Select--</option>
+                  <option>Student</option>
+                  <option>Professional Member</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group controlId="signup_address">
                 <Form.Label>Address</Form.Label>
                 <Form.Control
                   type="text"
                   value={address}
                   name="address"
-                  placeholder="Enter Name"
+                  placeholder="Enter Address"
                   onChange={signupValueChanged}
                 ></Form.Control>
               </Form.Group>
-              <Form.Group controlID="signup_contactno">
+              <Form.Group controlId="signup_contactno">
                 <Form.Label>Contact No</Form.Label>
                 <Form.Control
                   type="text"
@@ -60,19 +91,37 @@ const SignUp = () => {
                   onChange={signupValueChanged}
                 ></Form.Control>
               </Form.Group>
-              <Form.Group controlID="login_username">
-                <Form.Label>Username</Form.Label>
+              <Form.Group controlId="login_username">
+                <Form.Label>Email</Form.Label>
                 <Form.Control
                   type="text"
                   value={email}
                   name="email"
-                  placeholder="Enter UserName"
+                  placeholder="Enter email id"
                   onChange={signupValueChanged}
                 ></Form.Control>
               </Form.Group>
-              <Button onClick={next}>
-                Next
-              </Button>
+              <Form.Group controlId="login_username">
+                <Form.Label>Work_Place/University</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={workplace}
+                  name="workplace"
+                  placeholder="Enter email id"
+                  onChange={signupValueChanged}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="login_username">
+                <Form.Label>Designation</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={designation}
+                  name="designation"
+                  placeholder="Enter email id"
+                  onChange={signupValueChanged}
+                ></Form.Control>
+              </Form.Group>
+              <Button onClick={next}>Next</Button>
             </Form>
           </div>
         </Container>
