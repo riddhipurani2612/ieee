72798156db3bc8910404aa1bb536e4f4732f3277
Prem Raval <50078718+premraval1710@users.noreply.github.com>
2021-02-24 22:43:06 -0500

    Login Logic Commit

--- src/App.js
+++ src/App.js
@@ -2,40 +2,56 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navigation from "./components/Navigation";
 import Login from "./components/LoginForm";
-import Signup from './components/SignupForm';
+import Signup from "./components/SignupForm";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import FounderMembers from './components/FounderMember';
-import ProfessionalMembers from './components/ProfessionalMember';
-import StudentMembers from './components/StudentMember';
-import ChairsDesk from './components/ChairsDesk';
+import FounderMembers from "./components/FounderMember";
+import ProfessionalMembers from "./components/ProfessionalMember";
+import StudentMembers from "./components/StudentMember";
+import ChairsDesk from "./components/ChairsDesk";
 import AboutUs from "./components/About";
 import ContactUs from "./components/Contact";
-import Events from './components/Event';
-import Newsletter from './components/Newsletters';
-import Publication from './components/Publication';
-import Lecture from './components/Lecture';
+import Events from "./components/Event";
+import Newsletter from "./components/Newsletters";
+import Publication from "./components/Publication";
+import Lecture from "./components/Lecture";
 import SignUp from "./components/SignupForm";
 import SAR from "./components/SAR";
+import { useState } from "react";
 function App() {
+    const [isLoggedIn, setLoggedIn] = useState(false);
+
+    const setLogin = (state) => setLoggedIn(state);
+
     return (
         <Router>
             <div className="App">
-                <Navigation />
+                <Navigation isLoggedIn={isLoggedIn} />
                 <Switch>
                     <Route path="/" exact component={Home} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/foundermembers" component={FounderMembers}/>
-                    <Route path="/professionalmembers" component={ProfessionalMembers}/>
-                    <Route path="/studentmembers" component={StudentMembers}/>
-                    <Route path="/chairs-desk" component={ChairsDesk}/>
-                    <Route path="/about" component={AboutUs}/>
-                    <Route path="/contact" component={ContactUs}/>
+                    <Route
+                        path="/login"
+                        render={(routeProps) => (
+                            <Login
+                                routeProps={routeProps}
+                                setLogin={(state) => setLogin(state)}
+                            />
+                        )}
+                    />
+                    <Route path="/foundermembers" component={FounderMembers} />
+                    <Route
+                        path="/professionalmembers"
+                        component={ProfessionalMembers}
+                    />
+                    <Route path="/studentmembers" component={StudentMembers} />
+                    <Route path="/chairs-desk" component={ChairsDesk} />
+                    <Route path="/about" component={AboutUs} />
+                    <Route path="/contact" component={ContactUs} />
                     <Route path="/events" component={Events} />
-                    <Route path="/newsletters" component={Newsletter}/> 
-                    <Route path="/publication" component={Publication}/>    
-                    <Route path="/lecture" component={Lecture}/> 
-                    <Route path="/signup" component={SignUp}/>
-                    <Route path="/sar" component={SAR}/>
+                    <Route path="/newsletters" component={Newsletter} />
+                    <Route path="/publication" component={Publication} />
+                    <Route path="/lecture" component={Lecture} />
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/sar" component={SAR} />
                 </Switch>
                 <Footer />
             </div>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,68 +1,98 @@
 import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import styled from "styled-components";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
 const Styles = styled.div`
-  .main-bg {
-    background: #2e151b;
-    height: 100vh;
-  }
-  .text {
-    color: white;
-  }
+    .main-bg {
+        background: #2e151b;
+        height: 100vh;
+    }
+    .text {
+        color: white;
+    }
 `;
 
-const Login = () => {
-  const [formData, setFormData] = useState({});
+const Login = (props) => {
+    const [formData, setFormData] = useState({});
 
-  const valueChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+    const history = useHistory();
 
-  const { email, password } = formData;
+    const valueChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
 
-  const loginClicked = (e) => {
-    console.log(formData);
-  };
+    const { username, password } = formData;
 
-  return (
-    <Styles>
-      <div className="main-bg text">
-        <Container>
-          <br></br><br></br>
-          <div className="display-4 text-center my-5">
-            Login
-          </div>
-          <div>
-            <Form>
-              <Form.Group controlID="login_username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={email}
-                  name="email"
-                  placeholder="Enter UserName"
-                  onChange={valueChange}
-                ></Form.Control>
-              </Form.Group>
-              <Form.Group controlID="login_password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  value={password}
-                  placeholder="password"
-                  onChange={valueChange}
-                ></Form.Control>
-              </Form.Group>
-              <Button onClick={loginClicked}>Login</Button>
-            </Form>
-            <div className="text">
-                Do not have account? <a href="/signup" >Click here</a> to join!!
+    const loginClicked = async (e) => {
+        e.preventDefault();
+
+        let data = {
+            username,
+            password,
+        };
+
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        let response;
+        try {
+            response = await axios.post(
+                "http://localhost:5000/user/login",
+                data,
+                config
+            );
+            console.log(response.data);
+            localStorage.setItem("loggedInUserId", response.data._id);
+            props.setLogin(true);
+            history.push("/");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <Styles>
+            <div className="main-bg text">
+                <Container>
+                    <br></br>
+                    <br></br>
+                    <div className="display-4 text-center my-5">Login</div>
+                    <div>
+                        <Form>
+                            <Form.Group controlID="login_username">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    value={username}
+                                    name="username"
+                                    placeholder="Enter UserName"
+                                    onChange={valueChange}
+                                ></Form.Control>
+                            </Form.Group>
+                            <Form.Group controlID="login_password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    name="password"
+                                    value={password}
+                                    placeholder="password"
+                                    onChange={valueChange}
+                                ></Form.Control>
+                            </Form.Group>
+                            <Button onClick={loginClicked}>Login</Button>
+                        </Form>
+                        <div className="text">
+                            Do not have account?{" "}
+                            <a href="/signup">Click here</a> to join!!
+                        </div>
+                    </div>
+                </Container>
             </div>
-          </div>
-        </Container>
-      </div>
-    </Styles>
-  );
+        </Styles>
+    );
 };
 export default Login;
